53e460352cc3d69539263b904f9a1745f1657023
leonidn <leonid@twingo.co.il>
2015-02-02 19:54:13 +0200

    SQL for checking

--- SQLscripts/CDR_AGG.sql
+++ SQLscripts/CDR_AGG.sql
@@ -38,3 +38,24 @@ SEGMENTED BY hash(CDR_AGG.IP_source , CDR_AGG.IP_dest , CDR_AGG.Application_id) 
 
 
 SELECT MARK_DESIGN_KSAFE(1);
+
+ToDo:
+
+select
+                count(distinct partition_key) as part_num , node_name,sum(ros_row_count),
+                projection_name, table_schema
+                from partitions
+                where projection_name = 'CDR_b0'
+                group by projection_name, node_name,table_schema
+                order by node_name -- count(distinct partition_key) desc
+
+SELECT
+            NODE_NAME,
+            ros_count,
+            projection_name,
+            anchor_table_name,
+            projection_schema
+        FROM
+            projection_storage
+        ORDER BY
+            ros_count DESC;
